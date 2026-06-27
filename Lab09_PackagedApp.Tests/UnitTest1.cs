81f0749831ca349fba623c469854d264d840fe4f
radrut000 <radrut000@pbs.edu.pl>
2026-06-27 21:30:14 +0200

    Dodano projekt AppTests

---
+++ Lab09_PackagedApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab09_PackagedApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ TaskItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class TaskItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = "";
+    public bool IsCompleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
